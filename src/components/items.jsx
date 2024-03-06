aeb0dd21616982f62dac7165783752f0d1a16d46
Aubin <eyengadjou@gmail.com>
2024-03-06 01:33:14 +0100

    items

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,7 +1,52 @@
-function MyButton() {
-  return <button>J'suis le bouton</button>;
-}
+import { useState } from "react";
+import { CiEdit } from "react-icons/ci";
+import { FaTrash } from "react-icons/fa";
 
-export default function MyApp() {
-  return <>div</>;
+export default function Todo({ item, onUpdate, onDelete }) {
+  const [isEdit, setIsEdit] = useState(false);
+
+  function FormEdit() {
+    const [newValue, setNewValue] = useState(item.title);
+
+    function handleChange(e) {
+      setNewValue(e.target.value);
+    }
+
+    function handleClickUpdate(e) {
+      e.preventDefault();
+      onUpdate(item.id, newValue);
+      setIsEdit(false);
+    }
+
+    return (
+      <form onSubmit={handleClickUpdate}>
+        <input type="text" onChange={handleChange} defaultValue={newValue} />
+        <button type="submit">Modifier</button>
+      </form>
+    );
+  }
+
+  function TodoElement() {
+    return (
+      <>
+        <div key={item.id} className="title">
+          {item.title}
+        </div>
+        <div className="settings">
+          <button onClick={() => setIsEdit(true)}>
+            <CiEdit />
+          </button>
+          <button onClick={() => onDelete(item.id)}>
+            <FaTrash />
+          </button>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div className="todo">
+      <li className="cardTodo">{isEdit ? <FormEdit /> : <TodoElement />}</li>
+    </div>
+  );
 }
